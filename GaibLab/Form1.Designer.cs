a4e21213f4aa66d070868f5ebf7ea840e479e9b0
Andre Susanto <andre@andresusanto.info>
2015-10-12 10:40:07 +0700

    Init project

---
+++ GaibLab/Form1.Designer.cs
@@ -0,0 +1,245 @@
+﻿namespace GaibLab
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.Windows.Forms.ListViewItem listViewItem1 = new System.Windows.Forms.ListViewItem(new string[] {
+            "ContohTree.xlsx",
+            "Excel File"}, 1);
+            System.Windows.Forms.ListViewItem listViewItem2 = new System.Windows.Forms.ListViewItem(new string[] {
+            "Neural Network Introducion.pdf",
+            "PDF Document"}, 0);
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.nameFileHeader = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.imageList1 = new System.Windows.Forms.ImageList(this.components);
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.viewToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.tampilanLembarKerjaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.viewMenu1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.viewMenu2 = new System.Windows.Forms.ToolStripMenuItem();
+            this.viewMenu3 = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem1 = new System.Windows.Forms.ToolStripSeparator();
+            this.viewMenu4 = new System.Windows.Forms.ToolStripMenuItem();
+            this.pageSetupDialog1 = new System.Windows.Forms.PageSetupDialog();
+            this.treeView1 = new System.Windows.Forms.TreeView();
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // listView1
+            // 
+            this.listView1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.nameFileHeader,
+            this.columnHeader1});
+            this.listView1.Items.AddRange(new System.Windows.Forms.ListViewItem[] {
+            listViewItem1,
+            listViewItem2});
+            this.listView1.LargeImageList = this.imageList1;
+            this.listView1.Location = new System.Drawing.Point(6, 3);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(515, 374);
+            this.listView1.SmallImageList = this.imageList1;
+            this.listView1.TabIndex = 0;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.SmallIcon;
+            // 
+            // nameFileHeader
+            // 
+            this.nameFileHeader.Text = "Nama File";
+            this.nameFileHeader.Width = 200;
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "Jenis File";
+            this.columnHeader1.Width = 90;
+            // 
+            // imageList1
+            // 
+            this.imageList1.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("imageList1.ImageStream")));
+            this.imageList1.TransparentColor = System.Drawing.Color.Transparent;
+            this.imageList1.Images.SetKeyName(0, "pdf.jpg");
+            this.imageList1.Images.SetKeyName(1, "xlsx.PNG");
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem,
+            this.viewToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(742, 24);
+            this.menuStrip1.TabIndex = 1;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "&File";
+            // 
+            // viewToolStripMenuItem
+            // 
+            this.viewToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.tampilanLembarKerjaToolStripMenuItem});
+            this.viewToolStripMenuItem.Name = "viewToolStripMenuItem";
+            this.viewToolStripMenuItem.Size = new System.Drawing.Size(44, 20);
+            this.viewToolStripMenuItem.Text = "&View";
+            // 
+            // tampilanLembarKerjaToolStripMenuItem
+            // 
+            this.tampilanLembarKerjaToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.viewMenu1,
+            this.viewMenu2,
+            this.viewMenu3,
+            this.toolStripMenuItem1,
+            this.viewMenu4});
+            this.tampilanLembarKerjaToolStripMenuItem.Name = "tampilanLembarKerjaToolStripMenuItem";
+            this.tampilanLembarKerjaToolStripMenuItem.Size = new System.Drawing.Size(195, 22);
+            this.tampilanLembarKerjaToolStripMenuItem.Text = "Tampilan Lembar Kerja";
+            // 
+            // viewMenu1
+            // 
+            this.viewMenu1.Checked = true;
+            this.viewMenu1.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.viewMenu1.Name = "viewMenu1";
+            this.viewMenu1.Size = new System.Drawing.Size(150, 22);
+            this.viewMenu1.Tag = "1";
+            this.viewMenu1.Text = "Small Icons";
+            this.viewMenu1.Click += new System.EventHandler(this.ubahLembarKerja);
+            // 
+            // viewMenu2
+            // 
+            this.viewMenu2.Name = "viewMenu2";
+            this.viewMenu2.Size = new System.Drawing.Size(150, 22);
+            this.viewMenu2.Tag = "2";
+            this.viewMenu2.Text = "Medium Icons";
+            this.viewMenu2.Click += new System.EventHandler(this.ubahLembarKerja);
+            // 
+            // viewMenu3
+            // 
+            this.viewMenu3.Name = "viewMenu3";
+            this.viewMenu3.Size = new System.Drawing.Size(150, 22);
+            this.viewMenu3.Tag = "3";
+            this.viewMenu3.Text = "Large Icons";
+            this.viewMenu3.Click += new System.EventHandler(this.ubahLembarKerja);
+            // 
+            // toolStripMenuItem1
+            // 
+            this.toolStripMenuItem1.Name = "toolStripMenuItem1";
+            this.toolStripMenuItem1.Size = new System.Drawing.Size(147, 6);
+            // 
+            // viewMenu4
+            // 
+            this.viewMenu4.Name = "viewMenu4";
+            this.viewMenu4.Size = new System.Drawing.Size(150, 22);
+            this.viewMenu4.Tag = "4";
+            this.viewMenu4.Text = "Details";
+            this.viewMenu4.Click += new System.EventHandler(this.ubahLembarKerja);
+            // 
+            // treeView1
+            // 
+            this.treeView1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.treeView1.Location = new System.Drawing.Point(3, 3);
+            this.treeView1.Name = "treeView1";
+            this.treeView1.Size = new System.Drawing.Size(206, 371);
+            this.treeView1.TabIndex = 2;
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.splitContainer1.Location = new System.Drawing.Point(0, 27);
+            this.splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.treeView1);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.listView1);
+            this.splitContainer1.Size = new System.Drawing.Size(742, 377);
+            this.splitContainer1.SplitterDistance = 214;
+            this.splitContainer1.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(742, 404);
+            this.Controls.Add(this.splitContainer1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Laboratorium GaIB";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.PageSetupDialog pageSetupDialog1;
+        private System.Windows.Forms.ImageList imageList1;
+        private System.Windows.Forms.ToolStripMenuItem viewToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem tampilanLembarKerjaToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem viewMenu1;
+        private System.Windows.Forms.ToolStripMenuItem viewMenu2;
+        private System.Windows.Forms.ToolStripMenuItem viewMenu3;
+        private System.Windows.Forms.ToolStripSeparator toolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem viewMenu4;
+        private System.Windows.Forms.ColumnHeader nameFileHeader;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.TreeView treeView1;
+        private System.Windows.Forms.SplitContainer splitContainer1;
+    }
+}
+
---
+++ GaibLab/Form1.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Xml;
+using System.Windows.Forms;
+using System.IO;
+using System.Collections.Generic;
+
+namespace GaibLab
+{
+    public partial class Form1 : Form
+    {
+        string alamat = @"C:\Users\Andre\Documents\Visual Studio 2015\Projects\GaibLab\GaibLab\menu.xml";
+
+        public Form1()
+        {
+            InitializeComponent();
+
+
+            try
+            {
+                XmlDocument dom = new XmlDocument();
+                dom.Load(alamat);
+                
+                treeView1.Nodes.Clear();
+                AddNode(dom.DocumentElement, null);
+                treeView1.ExpandAll();
+            }
+            catch (XmlException xmlEx)
+            {
+                MessageBox.Show(xmlEx.Message);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+
+        }
+
+        private void AddNode(XmlNode inXmlNode, TreeNode inTreeNode)
+        {
+            XmlNode xNode;
+            TreeNode tNode;
+            XmlNodeList nodeList;
+            int i;
+
+            if (inXmlNode.HasChildNodes)
+            {
+                nodeList = inXmlNode.ChildNodes;
+                for (i = 0; i <= nodeList.Count - 1; i++)
+                {
+                    xNode = inXmlNode.ChildNodes[i];
+                    if (inTreeNode == null)
+                    {
+
+                        treeView1.Nodes.Add(new TreeNode(xNode.Name));
+                        tNode = treeView1.Nodes[i];
+                    }
+                    else
+                    {
+                        inTreeNode.Nodes.Add(new TreeNode(xNode.Name));
+                        tNode = inTreeNode.Nodes[i];
+                    }
+
+                    
+                    AddNode(xNode, tNode);
+                }
+            }
+            else
+            {
+                inTreeNode.Text = inXmlNode.Attributes["text"].InnerText.ToString();//(inXmlNode.OuterXml).Trim();
+            }
+        }
+
+        private void ubahLembarKerja(object sender, EventArgs e)
+        {
+            viewMenu1.Checked = false;
+            viewMenu2.Checked = false;
+            viewMenu3.Checked = false;
+            viewMenu4.Checked = false;
+
+
+            if (((ToolStripMenuItem)sender).Tag.Equals("1"))
+            {
+                listView1.View = View.SmallIcon;
+            }else if (((ToolStripMenuItem)sender).Tag.Equals("2"))
+            {
+                listView1.View = View.LargeIcon;
+            }
+            else if (((ToolStripMenuItem)sender).Tag.Equals("3"))
+            {
+                listView1.View = View.Tile;
+            }
+            else if (((ToolStripMenuItem)sender).Tag.Equals("4"))
+            {
+                listView1.View = View.Details;
+            }
+
+            ((ToolStripMenuItem)sender).Checked = true;
+            //listView1.View = 
+        }
+        
+    }
+}
